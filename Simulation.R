8867e3c2b46dd087f8ea7e57904600da7e465d3c
AngeloSci <angelo.scicolone2@gmail.com>
2017-02-25 10:36:43 +0000

    Added dynamic handling of mkt symbols + data manipulation

--- Simulation.R
+++ Simulation.R
@@ -8,13 +8,36 @@ today <- "2017-02-25"
 dayslag <- 360*5
 main_loc <- "D:/GithubLocal/mainworking/"
 filenm <- "pos_data.csv"
-
+mkt_sym = c("EUR/GBP","EUR/USD","USD/JPY","XAU/USD","USD/CHF")
 
 prvdate <- as.character(as.Date(today)-dayslag)
 pos_loc <- paste(main_loc,filenm, sep="")
 
-pos_data <- read.table(pos_loc, header = TRUE, sep = ",")
+pos_data <- data.frame(read.table(pos_loc, header = TRUE, sep = ","))
 
-getFX(c("EUR/GBP","EUR/USD","USD/JPY","XAU/USD","USD/CHF"),from=prvdate)
+
+instr <- getFX(mkt_sym,from=prvdate)
+
+mkt_list <- list()
+
+for (ii in 1:length(mkt_sym)){
+  mkt_list[[ii]] <- get(gsub("/","",mkt_sym[ii]))
+  mkt_list[[ii]] <- data.frame(dates = index(mkt_list[[ii]]),mkt_list[[ii]])
+}
+
+mkt_data <- data.frame(mkt_list[[1]])
+row.names(mkt_data) <- NULL
+
+for (ii in 3:(length(mkt_sym)+1)){
+  mkt_data[,ii] <- mkt_list[[ii-1]][match(mkt_data$dates,mkt_list[[ii-1]]$dates),2]
+}
+
+colnames(mkt_data) <-c("dates",gsub("/","",mkt_sym))
+
+for (ii in 1:length(instr)){
+  rm(list = instr[[ii]])
+}
+
+
 
 
